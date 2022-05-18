5fa3931279953a853e1fc56c97631e0d74d83c44
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-18 13:19:27 +0200

    controllers restaurants & reviews

--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_092514) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "phone_number"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+asiat = { name: 'Pierre Sang', address: '55 Rue Oberkampf - 75011 Paris', category: 'chinese' }
+pizza = { name: 'Chez Justine', address: '96 Rue Oberkampf - 75011 Paris', category: 'italian' }
+sushi = { name: 'Nakagawa', address: '142 Rue Oberkampf - 75011 Paris', category: 'japanese' }
+burgers = { name: 'Le Camion Qui Fume', address: '66 Rue Oberkampf - 75011 Paris', category: 'french' }
+pny = { name: 'PNY', address: '96 Rue Oberkampf - 75011 Paris', category: 'belgian' }
+
+[asiat, pizza, sushi, burgers, pny].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name} - #{restaurant.category}"
+end
+puts 'Finished!'
