a323c0a111ba9b208228671ed16e718ce1826434
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-18 17:58:46 +0200

    Refactoring controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'restaurants#index'
   resources :restaurants, only: %i[index show new create] do
-    resources :reviews, only: %i[new create]
+    resources :reviews, only: %i[create]
   end
 end
