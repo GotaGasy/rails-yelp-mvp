57044f1e530c3e87b61e98176e432d953b051453
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-18 16:36:42 +0200

    App restaurant & review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, presence: true, inclusion: %w[Chinese Italian Japanese French Belgian chinese italian japanese french belgian]
+  validates :category, presence: true, inclusion:
+  %w[Chinese Italian Japanese French Belgian chinese italian japanese french belgian]
 end
